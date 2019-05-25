533cdcd12ca7d08703343de99d5780938658f3bd
Khalyomede <khalyomede@gmail.com>
2019-05-25 15:21:59 +0200

    enable logs

--- example/example-5.js
+++ example/example-5.js
@@ -4,6 +4,7 @@ const prerendering = new Prerendering();
 
 prerendering.setBaseUrl("http://example.com");
 prerendering.setFolderPath("prerendered");
+prerendering.setDebugMode(true);
 prerendering.addRoute(new Route().setUrl("/about/us.html"));
 
 prerendering.start();
